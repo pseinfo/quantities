a7f928b4b4a33e948f82d7621133166ae8672885
komed3 <pk97@outlook.de>
2026-05-04 10:07:03 +0200

    reduced planck const

--- src/data/constant/reducedPlanckConstant.ts
+++ src/data/constant/reducedPlanckConstant.ts
@@ -1,18 +1,18 @@
 import type { ConstantConfig } from '../../types/constant';
 
 export default ( {
-  id: 'planckConstant',
+  id: 'reducedPlanckConstant',
   symbol: {
-    plain: 'ℎ',
-    latex: 'h'
+    plain: 'ℏ',
+    latex: '\hbar'
   },
   name: {
-    en: 'Planck constant',
-    de: 'Planck-Konstante'
+    en: 'Reduced Planck constant',
+    de: 'Reduzierte Planck-Konstante'
   },
-  value: 6.62607015e-34,
+  value: 1.054571817e-34,
   relUncertainty: 0,
   vector: [ -1, 2, 1, 0, 0, 0, 0 ],
   quantity: 'action',
-  unit: 'joulePerHertz'
+  unit: 'jouleSecond'
 } ) as const satisfies ConstantConfig;
