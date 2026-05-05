47d7501fa3a2646788236cfa743094713273575f
komed3 <pk97@outlook.de>
2026-05-05 22:55:46 +0200

    add reduced Planck const

---
+++ src/data/constant/reducedPlanckConstant.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'reducedPlanckConstant',
+  symbol: {
+    plain: 'ℏ',
+    latex: '\hbar'
+  },
+  name: {
+    en: 'reduced Planck constant',
+    de: 'Dirac-Konstante'
+  },
+  vector: [ -1, 2, 1, 0, 0, 0, 0 ],
+  value: 1.054571817e-34
+} ) as const;
