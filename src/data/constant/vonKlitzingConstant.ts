93a2d2b3059d1da7d5b1c9f12a403f593d1836ea
komed3 <pk97@outlook.de>
2026-05-05 23:59:23 +0200

    update data

---
+++ src/data/constant/vonKlitzingConstant.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'vonKlitzingConstant',
+  symbol: {
+    plain: 'RK',
+    latex: 'R_{\\mathrm{K}}'
+  },
+  name: {
+    en: 'von Klitzing constant',
+    de: 'Von-Klitzing-Konstante'
+  },
+  vector: [ -3, 2, 1, -2, 0, 0, 0 ],
+  value: 25812.80745
+} ) as const;
