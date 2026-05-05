715d64288791c0c2c316538f13f1a1b9a2ca3eaf
komed3 <pk97@outlook.de>
2026-05-05 23:48:59 +0200

    add Wien entropy displacement law constant

---
+++ src/data/constant/wienEntropyConstant.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'wienEntropyConstant',
+  symbol: {
+    plain: 'bEntropy',
+    latex: 'b_{\\mathrm{entropy}}'
+  },
+  name: {
+    en: 'Wien entropy displacement law constant',
+    de: 'Wiensche Entropiekonstante'
+  },
+  vector: [ 0, 1, 0, 0, 1, 0, 0 ],
+  value: 3.002916077e-3
+} ) as const;
