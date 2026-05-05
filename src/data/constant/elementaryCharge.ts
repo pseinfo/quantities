46b0b67a732767029f62cfb1880929231bb0c8b5
komed3 <pk97@outlook.de>
2026-05-05 23:50:28 +0200

    add elementary charge

---
+++ src/data/constant/elementaryCharge.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'elementaryCharge',
+  symbol: {
+    plain: 'e',
+    latex: 'e'
+  },
+  name: {
+    en: 'elementary charge',
+    de: 'Elementarladung'
+  },
+  vector: [ 1, 0, 0, 1, 0, 0, 0 ],
+  value: 1.602176634e-19
+} ) as const;
