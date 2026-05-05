bd04797b2b3e824285fcdf41c346380faa40385e
komed3 <pk97@outlook.de>
2026-05-05 22:59:35 +0200

    add Boltzmann const.

---
+++ src/data/constant/boltzmannConstant.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'boltzmannConstant',
+  symbol: {
+    plain: 'kB',
+    latex: 'k_{\mathrm {B}}'
+  },
+  name: {
+    en: 'Boltzmann constant',
+    de: 'Boltzmann-Konstante'
+  },
+  vector: [ -2, 2, 1, 0, -1, 0, 0 ],
+  value: 1.380649e-23
+} ) as const;
