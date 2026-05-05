175d45d0c08ac862139af673df3be09abcb25adc
komed3 <pk97@outlook.de>
2026-05-05 23:18:28 +0200

    add 1st radiation const.

---
+++ src/data/constant/firstRadiationConstant.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'firstRadiationConstant',
+  symbol: {
+    plain: 'c1',
+    latex: 'c_{1}'
+  },
+  name: {
+    en: 'first radiation constant',
+    de: '1. Strahlungskonstante'
+  },
+  vector: [ -3, 4, 1, 0, 0, 0, 0 ],
+  value: 3.741771852e-16
+} ) as const;
