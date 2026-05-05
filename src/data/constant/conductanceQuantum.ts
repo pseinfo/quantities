d6234079a9f5515e9d0cf1eff318ad801e69ada0
komed3 <pk97@outlook.de>
2026-05-05 23:52:35 +0200

    add conductance quantum

---
+++ src/data/constant/conductanceQuantum.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'conductanceQuantum',
+  symbol: {
+    plain: 'G0',
+    latex: 'G_{0}'
+  },
+  name: {
+    en: 'conductance quantum',
+    de: 'Quantelung des Leitwerts'
+  },
+  vector: [ 3, -2, -1, 2, 0, 0, 0 ],
+  value: 7.748091729e-5
+} ) as const;
