9e901ee75fbfbb69da9e117845aec7ce56f03462
komed3 <pk97@outlook.de>
2026-05-05 23:37:07 +0200

    add first radiation constant for spectral radiance

---
+++ src/data/constant/firstRadiationConstantSpectral.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'firstRadiationConstantSpectral',
+  symbol: {
+    plain: 'c1L',
+    latex: 'c_{\mathrm {1L}}'
+  },
+  name: {
+    en: 'first radiation constant for spectral radiance',
+    de: '1. Strahlungskonstante für die spektrale Strahlungsintensität'
+  },
+  vector: [ -3, 4, 1, 0, 0, 0, 0 ],
+  value: 1.191042972e-16
+} ) as const;
