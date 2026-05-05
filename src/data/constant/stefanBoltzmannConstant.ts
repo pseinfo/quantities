ab1eff46039aeacb3cb2dbd0ce6a0e3ab6830e91
komed3 <pk97@outlook.de>
2026-05-05 23:13:52 +0200

    Create stefanBoltzmannConstant.ts

---
+++ src/data/constant/stefanBoltzmannConstant.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'stefanBoltzmannConstant',
+  symbol: {
+    plain: 'σ',
+    latex: '\sigma'
+  },
+  name: {
+    en: 'Stefan–Boltzmann constant',
+    de: 'Stefan-Boltzmann-Konstante'
+  },
+  vector: [ -3, 0, 1, 0, -4, 0, 0 ],
+  value: 5.670374419e-8
+} ) as const;
