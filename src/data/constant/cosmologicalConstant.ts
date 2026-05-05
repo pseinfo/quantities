19e8080bbe578c11dde9de41af9c55f5ddc5b932
komed3 <pk97@outlook.de>
2026-05-05 23:06:48 +0200

    add cosmological const.

---
+++ src/data/constant/cosmologicalConstant.ts
@@ -0,0 +1,14 @@
+export default ( {
+  id: 'cosmologicalConstant',
+  symbol: {
+    plain: 'Λ',
+    latex: '\Lambda'
+  },
+  name: {
+    en: 'Cosmological constant',
+    de: 'Kosmologische Konstante'
+  },
+  vector: [ 0, -2, 0, 0, 0, 0, 0 ],
+  value: 1.08929e-52,
+  uncertainty: 0.027
+} ) as const;
