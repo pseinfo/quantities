0e3243cce3c63b9ee4f5f69efbb15831cba19cfc
komed3 <pk97@outlook.de>
2026-05-05 22:51:30 +0200

    Create planckConstant.ts

---
+++ src/data/constant/planckConstant.ts
@@ -0,0 +1,13 @@
+export default ( {
+  id: 'planckConstant',
+  symbol: {
+    plain: 'ℎ',
+    latex: 'h'
+  },
+  name: {
+    en: 'Planck constant',
+    de: 'Plank-Konstante'
+  },
+  vector: [ -1, 2, 1, 0, 0, 0, 0 ],
+  value: 6.62607015e-34
+} ) as const;
