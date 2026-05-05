93d923fd4034de5f2fe280255ac46163e50a4338
komed3 <pk97@outlook.de>
2026-05-05 22:47:25 +0200

    define vaccum light speed

--- src/data/constant/vacuumLightSpeed.ts
+++ src/data/constant/vacuumLightSpeed.ts
@@ -1,5 +1,13 @@
 export default ( {
   id: 'vacuumLightSpeed',
+  symbol: {
+    plain: 'c',
+    latex: 'c'
+  },
+  name: {
+    en: 'speed of light in vacuum',
+    de: 'Vakuumlichtgeschwindigkeit'
+  },
   vector: [ -1, 1, 0, 0, 0, 0, 0 ],
   value: 299792458
 } ) as const;
