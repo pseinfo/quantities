3bfdd528676a47fa74140f21372bd891456f8994
komed3 <pk97@outlook.de>
2026-05-06 19:42:05 +0200

    use different systems for dim vectors

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,9 @@
-export type Dimension = readonly [
+export enum System {
+  SI = 'SI',
+  CGS = 'CGS'
+}
+
+export type SIDimension = readonly [
   T: number, // time
   L: number, // length
   M: number, // mass
@@ -7,3 +12,14 @@ export type Dimension = readonly [
   N: number, // amount
   J: number  // luminous intensity
 ];
+
+export type CGSDimension = readonly [
+  T: number, // time
+  L: number, // length
+  M: number, // mass
+];
+
+export type Dimension< S extends System > =
+  S extends System.SI ? SIDimension :
+  S extends System.CGS ? CGSDimension :
+  never;
