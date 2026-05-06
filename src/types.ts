72a0fc82476959a90701bf21de5c33870a59ec82
komed3 <pk97@outlook.de>
2026-05-06 19:36:08 +0200

    declare dimension vector as base

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type Dimension = readonly [
+  T: number, // time
+  L: number, // length
+  M: number, // mass
+  I: number, // electric current
+  θ: number, // temperature
+  N: number, // amount
+  J: number  // luminous intensity
+];
