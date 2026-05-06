35783093639c9b2dafa871a2e5ef11928897111b
komed3 <pk97@outlook.de>
2026-05-06 23:40:07 +0200

    declare Kelvin

---
+++ src/registry/units/kelvin.ts
@@ -0,0 +1,22 @@
+import type { Unit, UnitId } from '../../types';
+
+export default ( {
+  id: 'K' as UnitId,
+  dim: [ 0, 0, 0, 0, 1, 0, 0 ],
+  structure: [ {
+    unit: 'K' as UnitId,
+    exponent: 1
+  } ],
+  conversion: {
+    scale: 1
+  },
+  prefixable: false,
+  meta: {
+    symbol: 'K',
+    latex: '\\mathrm{K}',
+    name: {
+      en: 'Kelvin',
+      de: 'Kelvin'
+    }
+  }
+} ) as const satisfies Unit;
