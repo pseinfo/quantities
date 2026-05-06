86620577b3ae2242c977bbd32ebb540b633180e1
komed3 <pk97@outlook.de>
2026-05-06 23:38:01 +0200

    declare "gram" unit

---
+++ src/registry/units/gram.ts
@@ -0,0 +1,22 @@
+import type { Unit, UnitId } from '../../types';
+
+export default ( {
+  id: 'g' as UnitId,
+  dim: [ 0, 0, 1, 0, 0, 0, 0 ],
+  structure: [ {
+    unit: 'g' as UnitId,
+    exponent: 1
+  } ],
+  conversion: {
+    scale: 0.001
+  },
+  prefixable: true,
+  meta: {
+    symbol: 'g',
+    latex: '\\mathrm{g}',
+    name: {
+      en: 'gram',
+      de: 'Gramm'
+    }
+  }
+} ) as const satisfies Unit;
