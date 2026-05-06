ab734c90d06549fd793f56cfea05004905665296
komed3 <pk97@outlook.de>
2026-05-06 23:49:47 +0200

    Create ampere.ts

---
+++ src/registry/units/ampere.ts
@@ -0,0 +1,22 @@
+import type { Unit, UnitId } from '../../types';
+
+export default ( {
+  id: 'A' as UnitId,
+  dim: [ 0, 0, 0, 1, 0, 0, 0 ],
+  structure: [ {
+    unit: 'A' as UnitId,
+    exponent: 1
+  } ],
+  conversion: {
+    scale: 1
+  },
+  prefixable: true,
+  meta: {
+    symbol: 'A',
+    latex: '\\mathrm{A}',
+    name: {
+      en: 'Ampere',
+      de: 'Ampere'
+    }
+  }
+} ) as const satisfies Unit;
