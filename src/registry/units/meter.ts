c2dd32345dd4cfb55ff73cb47e5c7b8e44d97f75
komed3 <pk97@outlook.de>
2026-05-06 23:33:29 +0200

    define meter unit

---
+++ src/registry/units/meter.ts
@@ -0,0 +1,21 @@
+import type { Unit, UnitId } from '../../types';
+
+export default ( {
+  id: 'm' as UnitId,
+  dim: [ 0, 1, 0, 0, 0, 0, 0 ],
+  structure: [ {
+    unit: 'm' as UnitId,
+    exponent: 1
+  } ],
+  conversion: {
+    scale: 1
+  },
+  meta: {
+    symbol: 'm',
+    latex: '\\mathrm{m}',
+    name: {
+      en: 'meter',
+      de: 'Meter'
+    }
+  }
+} ) as const satisfies Unit;
