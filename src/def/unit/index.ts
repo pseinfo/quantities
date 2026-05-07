ae57895dc30f9e57eb2460906d11c3cebd5bc4cc
komed3 <pk97@outlook.de>
2026-05-07 20:31:45 +0200

    register allready declared units

--- src/def/unit/index.ts
+++ src/def/unit/index.ts
@@ -1,10 +1,41 @@
 import type { UnitRegistry } from '../../types/registry';
 
+import { atomicMass } from './atomic/atomicMass';
+import { bohrRadius } from './atomic/bohrRadius';
+
 import { gram } from './base/gram';
 import { kelvin } from './base/kelvin';
 import { meter } from './base/meter';
 import { second } from './base/second';
 
+import { degC } from './common/temperature/degCelsius';
+import { degF } from './common/temperature/degFahrenheit';
+import { degR } from './common/temperature/degRankine';
+
+import { day } from './common/time/day';
+import { hour } from './common/time/hour';
+import { min } from './common/time/minute';
+import { year } from './common/time/year';
+
+import { planckLength } from './planck/planckLength';
+import { planckMass } from './planck/planckMass';
+import { planckTemp } from './planck/planckTemp';
+import { planckTime } from './planck/planckTime';
+
 export default new Set( [
-  second, meter, gram, kelvin
+  // SI base units
+  gram, kelvin, meter, second,
+  // SI derived units
+  // Imperial units
+  // USC units
+  // CGS units
+  // Gaussian units
+  // Natural units
+  // Atomic units
+  atomicMass, bohrRadius,
+  // Planck units
+  planckLength, planckMass, planckTemp, planckTime,
+  // Astronomical units
+  // Common units
+  degC, degF, degR, day, hour, min, year
 ] ) satisfies UnitRegistry;
