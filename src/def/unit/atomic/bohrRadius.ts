e0989296c55b6d766d67f4b1c9fc7578ef937913
komed3 <pk97@outlook.de>
2026-05-07 20:10:25 +0200

    implement atomic units

---
+++ src/def/unit/atomic/bohrRadius.ts
@@ -0,0 +1,34 @@
+import type { LengthDim } from '../../../types/dimension';
+import type { UnitDef, UnitRef } from '../../../types/unit';
+
+export const bohrRadius = 'a0' as UnitRef< LengthDim, 'a0' >;
+
+export default ( {
+  id: bohrRadius,
+  dim: [ 0, 1, 0, 0, 0, 0, 0 ],
+  structure: [],
+  conversion: {
+    factor: 5.2917721054482e-11,
+    uncertainty: 1.6e-10
+  },
+  prefixable: false,
+  unitSystem: [ 'atomic' ],
+  category: [ 'accepted' ],
+  aliases: [ 'bohr radius' ],
+  meta: {
+    symbol: {
+      default: {
+        ascii: 'a0',
+        latex: 'a_{0}'
+      }
+    },
+    name: {
+      en: [ 'Bohr radius' ],
+      de: [ 'Bohrscher Radius' ]
+    },
+    description: {
+      en: 'Mean distance between the nucleus and the electron in a hydrogen atom in its ground state; atomic units',
+      de: 'Mittlere Entfernung zwischen Kern und Elektron im Grundzustand eines Wasserstoffatoms; Atomare Einheiten'
+    }
+  }
+} ) as const satisfies UnitDef< LengthDim, typeof bohrRadius >;
