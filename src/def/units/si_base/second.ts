5f330302233f0f9b36a3f0a6733e03bfc6ccdded
komed3 <pk97@outlook.de>
2026-05-07 09:52:29 +0200

    declare important unit def props.

--- src/def/units/si_base/second.ts
+++ src/def/units/si_base/second.ts
@@ -1,1 +1,9 @@
-import type { UnitId } from '../../../types/unit';
+import type { UnitDef, UnitId } from '../../../types/unit';
+
+export default ( {
+  id: 's' as UnitId< 's' >,
+  dim: [ 1, 0, 0, 0, 0, 0, 0 ],
+  structure: [],
+  conversion: 1,
+  prefixable: true
+} ) as const satisfies UnitDef;
--- src/types/unit.d.ts
+++ src/types/unit.d.ts
@@ -1,1 +1,22 @@
-export type UnitId< ID extends string = string > = ID & { readonly __brand: 'unitId' };
+import type { Dimension } from './common';
+
+export type UnitId< ID extends string > = ID & { readonly __brand: 'unitId' };
+
+export type UnitStruct = Array< {
+  unit: UnitDef;
+  exp: number;
+  prefix?: any;
+} >;
+
+export type UnitConv =
+  | { factor: number; }
+  | { scale: number; offset: number }
+  | 1;
+
+export type UnitDef< ID extends string = string, D extends Dimension = Dimension > = {
+  id: UnitId< ID >;
+  dim: D;
+  structure: UnitStruct;
+  conversion: UnitConv;
+  prefixable: boolean;
+};
