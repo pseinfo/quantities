a5658d2a050908c51011af40d4d706f718130a74
komed3 <pk97@outlook.de>
2026-05-08 22:11:03 +0200

    Create ampere.ts

---
+++ src/def/unit/si/base/ampere.ts
@@ -0,0 +1,43 @@
+import type { ElectricCurrentDim } from '../../../../types/dimension';
+import type { UnitDef, UnitRef } from '../../../../types/unit';
+
+export const ampere = 'A' as UnitRef< ElectricCurrentDim, 'A' >;
+
+export default ( {
+  id: ampere,
+  dim: [ 0, 0, 0, 1, 0, 0, 0 ],
+  structure: [],
+  conversion: 1,
+  prefixable: true,
+  aliases: [ 'ampere', 'amperes' ],
+  context: {
+    system: [ 'si' ],
+    kind: 'base',
+    status: 'active',
+    property: {
+      dimensionless: false,
+      logarithmic: false
+    }
+  },
+  meta: {
+    symbol: [ {
+      id: 'A',
+      canonical: true,
+      context: {
+        system: [ 'si' ],
+      },
+      format: {
+        plain: 'A',
+        latex: '\\mathrm{A}'
+      }
+    } ],
+    name: {
+      en: [ 'ampere', 'amperes' ],
+      de: [ 'Ampere' ]
+    },
+    description: {
+      en: 'SI base unit of electric current',
+      de: 'SI-Basiseinheit der elektrischen Stromstärke'
+    }
+  }
+} ) as const satisfies UnitDef< ElectricCurrentDim, typeof ampere >;
