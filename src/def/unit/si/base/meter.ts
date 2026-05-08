d4f3c077ad2ee7fa4f9e3b888a3378f2e46cd264
komed3 <pk97@outlook.de>
2026-05-08 22:03:56 +0200

    Create meter.ts

---
+++ src/def/unit/si/base/meter.ts
@@ -0,0 +1,43 @@
+import type { LengthDim } from '../../../../types/dimension';
+import type { UnitDef, UnitRef } from '../../../../types/unit';
+
+export const meter = 'm' as UnitRef< LengthDim, 'm' >;
+
+export default ( {
+  id: meter,
+  dim: [ 0, 1, 0, 0, 0, 0, 0 ],
+  structure: [],
+  conversion: 1,
+  prefixable: true,
+  aliases: [ 'meter', 'meters', 'metre', 'metres' ],
+  context: {
+    system: [ 'si' ],
+    kind: 'base',
+    status: 'active',
+    property: {
+      dimensionless: false,
+      logarithmic: false
+    }
+  },
+  meta: {
+    symbol: [ {
+      id: 'm',
+      canonical: true,
+      context: {
+        system: [ 'si' ],
+      },
+      format: {
+        plain: 'm',
+        latex: '\\mathrm{m}'
+      }
+    } ],
+    name: {
+      en: [ 'meter', 'meters' ],
+      de: [ 'Meter' ]
+    },
+    description: {
+      en: 'SI base unit of length',
+      de: 'SI-Basiseinheit der Länge'
+    }
+  }
+} ) as const satisfies UnitDef< LengthDim, typeof meter >;
