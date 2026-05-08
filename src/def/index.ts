3a845cdd676b75476f5573afb6fbc12db6595520
komed3 <pk97@outlook.de>
2026-05-08 16:34:44 +0200

    remove old unit/prefix implementation

--- src/def/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Registry } from '../types/registry';
-
-import prefixRegistry from './prefix';
-import unitRegistry from './unit';
-
-export default ( {
-  unit: unitRegistry,
-  prefix: prefixRegistry
-} ) as const satisfies Registry;
--- src/def/prefix/index.ts
+++
@@ -1,8 +0,0 @@
-import type { PrefixRegistry } from '../../types/registry';
-
-import { hecto } from './hecto';
-import { kilo } from './kilo';
-
-export default ( [
-  hecto, kilo
-] ) as const satisfies PrefixRegistry;
