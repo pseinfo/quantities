29c120806a7264443e4c71118cc919559c9feb4f
komed3 <pk97@outlook.de>
2026-05-09 00:38:58 +0200

    Update def/unit/index.ts

--- src/def/unit/index.ts
+++ src/def/unit/index.ts
@@ -1,5 +1,9 @@
 import type { UnitRegistry } from '../../types/registry';
 
+import { astronomicalUnit } from './astronomical/astronomicalUnit';
+import { lightYear } from './astronomical/lightYear';
+import { parsec } from './astronomical/parsec';
+
 import { ampere } from './si/base/ampere';
 import { candela } from './si/base/candela';
 import { gram } from './si/base/gram';
@@ -10,5 +14,7 @@ import { second } from './si/base/second';
 
 export default ( [
   // SI base units
-  ampere, candela, gram, kelvin, meter, mole, second
+  ampere, candela, gram, kelvin, meter, mole, second,
+  // Astronomical units
+  astronomicalUnit, lightYear, parsec
 ] ) as const satisfies UnitRegistry;
