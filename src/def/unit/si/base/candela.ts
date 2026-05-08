2588ae584214d3e796e2b87330bf5a005153fe30
komed3 <pk97@outlook.de>
2026-05-08 22:30:17 +0200

    Create candela.ts

---
+++ src/def/unit/si/base/candela.ts
@@ -0,0 +1,43 @@
+import type { LuminousIntensityDim } from '../../../../types/dimension';
+import type { UnitDef, UnitRef } from '../../../../types/unit';
+
+export const candela = 'cd' as UnitRef< LuminousIntensityDim, 'cd' >;
+
+export default ( {
+  id: candela,
+  dim: [ 0, 0, 0, 0, 0, 0, 1 ],
+  structure: [],
+  conversion: 1,
+  prefixable: true,
+  aliases: [ 'candela', 'candelas' ],
+  context: {
+    system: [ 'si' ],
+    kind: 'base',
+    status: 'active',
+    property: {
+      dimensionless: false,
+      logarithmic: false
+    }
+  },
+  meta: {
+    symbol: [ {
+      id: 'cd',
+      canonical: true,
+      context: {
+        system: [ 'si' ],
+      },
+      format: {
+        plain: 'cd',
+        latex: '\\mathrm{cd}'
+      }
+    } ],
+    name: {
+      en: [ 'candela', 'candelas' ],
+      de: [ 'Candela' ]
+    },
+    description: {
+      en: 'SI base unit of luminous intensity',
+      de: 'SI-Basiseinheit der Lichtstärke'
+    }
+  }
+} ) as const satisfies UnitDef< LuminousIntensityDim, typeof candela >;
