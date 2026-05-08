45e21eda75ea88c8e286c38d0dda7c57c36fdeab
komed3 <pk97@outlook.de>
2026-05-08 22:03:47 +0200

    Create second.ts

---
+++ src/def/unit/si/base/second.ts
@@ -0,0 +1,43 @@
+import type { TimeDim } from '../../../../types/dimension';
+import type { UnitDef, UnitRef } from '../../../../types/unit';
+
+export const second = 's' as UnitRef< TimeDim, 's' >;
+
+export default ( {
+  id: second,
+  dim: [ 1, 0, 0, 0, 0, 0, 0 ],
+  structure: [],
+  conversion: 1,
+  prefixable: true,
+  aliases: [ 'sec', 'second', 'seconds' ],
+  context: {
+    system: [ 'si' ],
+    kind: 'base',
+    status: 'active',
+    property: {
+      dimensionless: false,
+      logarithmic: false
+    }
+  },
+  meta: {
+    symbol: [ {
+      id: 's',
+      canonical: true,
+      context: {
+        system: [ 'si' ],
+      },
+      format: {
+        plain: 's',
+        latex: '\\mathrm{s}'
+      }
+    } ],
+    name: {
+      en: [ 'second', 'seconds' ],
+      de: [ 'Sekunde', 'Sekunden' ]
+    },
+    description: {
+      en: 'SI base unit of time',
+      de: 'SI-Basiseinheit der Zeit'
+    }
+  }
+} ) as const satisfies UnitDef< TimeDim, typeof second >;
