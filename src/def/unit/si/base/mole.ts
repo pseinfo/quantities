749bfa78366f9f8813e4cd5bff9296604eea66e1
komed3 <pk97@outlook.de>
2026-05-08 22:24:43 +0200

    Create mole.ts

---
+++ src/def/unit/si/base/mole.ts
@@ -0,0 +1,43 @@
+import type { AmountOfSubstanceDim } from '../../../../types/dimension';
+import type { UnitDef, UnitRef } from '../../../../types/unit';
+
+export const mole = 'mol' as UnitRef< AmountOfSubstanceDim, 'mol' >;
+
+export default ( {
+  id: mole,
+  dim: [ 0, 0, 0, 0, 0, 1, 0 ],
+  structure: [],
+  conversion: 1,
+  prefixable: true,
+  aliases: [ 'mole', 'moles' ],
+  context: {
+    system: [ 'si' ],
+    kind: 'base',
+    status: 'active',
+    property: {
+      dimensionless: false,
+      logarithmic: false
+    }
+  },
+  meta: {
+    symbol: [ {
+      id: 'mol',
+      canonical: true,
+      context: {
+        system: [ 'si' ],
+      },
+      format: {
+        plain: 'mol',
+        latex: '\\mathrm{mol}'
+      }
+    } ],
+    name: {
+      en: [ 'mole', 'moles' ],
+      de: [ 'Mol' ]
+    },
+    description: {
+      en: 'SI base unit of the amount of substance',
+      de: 'SI-Basiseinheit der Stoffmenge'
+    }
+  }
+} ) as const satisfies UnitDef< AmountOfSubstanceDim, typeof mole >;
