d647479bf3fee421b238d689df4cc98d65632a39
komed3 <pk97@outlook.de>
2026-05-08 22:17:17 +0200

    Create kelvin.ts

---
+++ src/def/unit/si/base/kelvin.ts
@@ -0,0 +1,43 @@
+import type { TemperatureDim } from '../../../../types/dimension';
+import type { UnitDef, UnitRef } from '../../../../types/unit';
+
+export const kelvin = 'K' as UnitRef< TemperatureDim, 'K' >;
+
+export default ( {
+  id: kelvin,
+  dim: [ 0, 0, 0, 0, 1, 0, 0 ],
+  structure: [],
+  conversion: 1,
+  prefixable: true,
+  aliases: [ 'kelvin' ],
+  context: {
+    system: [ 'si' ],
+    kind: 'base',
+    status: 'active',
+    property: {
+      dimensionless: false,
+      logarithmic: false
+    }
+  },
+  meta: {
+    symbol: [ {
+      id: 'K',
+      canonical: true,
+      context: {
+        system: [ 'si' ],
+      },
+      format: {
+        plain: 'K',
+        latex: '\\mathrm{K}'
+      }
+    } ],
+    name: {
+      en: [ 'Kelvin' ],
+      de: [ 'Kelvin' ]
+    },
+    description: {
+      en: 'SI base unit of thermodynamic temperature',
+      de: 'SI-Basiseinheit der thermodynamischen Temperatur'
+    }
+  }
+} ) as const satisfies UnitDef< TemperatureDim, typeof kelvin >;
