199c46c3976ee7a2eedf3a569a970c260f8776a8
komed3 <pk97@outlook.de>
2026-05-15 16:07:28 +0200

    add dictionary for enums

---
+++ src/types/dict.ts
@@ -0,0 +1,47 @@
+export enum Lang {
+  EN = 'en',
+  DE = 'de'
+}
+
+export enum Format {
+  PLAIN = 'plain',
+  UNICODE = 'unicode',
+  LATEX = 'latex'
+}
+
+export enum UnitSystem {
+  COMMON = 'common',
+  SI = 'si',
+  CGS = 'cgs',
+  GAUSSIAN = 'cgs_gaussian',
+  EMU = 'cgs_emu',
+  ESU = 'cgs_esu',
+  NATURAL = 'natural',
+  PLANCK = 'natural_planck',
+  ATOMIC = 'natural_atomic',
+  STONEY = 'natural_stoney',
+  IMPERIAL = 'imperial',
+  USC = 'usc',
+  ASTRO = 'astronomical'
+}
+
+export enum Branch {
+  COMMON = 'common',
+  MECHANICS = 'mechanics',
+  ACOUSTICS = 'acoustics',
+  ELECTROMAGNETISM = 'electromagnetism',
+  OPTICS = 'optics',
+  THERMODYNAMICS = 'thermodynamics'
+}
+
+export enum UnitSIType {
+  BASE = 'base',
+  DERIVED = 'derived',
+  ACCEPTED = 'accepted'
+}
+
+export enum UnitStatus {
+  ACTIVE = 'active',
+  HISTORICAL = 'historical',
+  OBSOLETE = 'obsolete'
+}
