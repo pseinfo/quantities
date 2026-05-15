54bb8cf3e8740e0952b2fa716ec0a04d86c071b6
komed3 <pk97@outlook.de>
2026-05-15 16:05:43 +0200

    delete all old stuff

--- src/def/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Registry } from '../types/registry';
-
-import prefix from './prefix';
-import quantity from './quantity';
-import unit from './unit';
-
-export default ( { quantity, unit, prefix } ) as const satisfies Registry;
--- src/def/quantity/index.ts
+++
@@ -1,8 +0,0 @@
-import type { QuantityRegistry } from '../../types/registry';
-
-import { time } from './base/time';
-
-export default ( [
-  // Base quantities
-  time
-] ) as const satisfies QuantityRegistry;
