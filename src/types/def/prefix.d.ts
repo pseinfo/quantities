429812fb3bb7c950cd4a028078fec2ca8410d568
komed3 <pk97@outlook.de>
2026-05-15 16:09:59 +0200

    Create prefix.d.ts

---
+++ src/types/def/prefix.d.ts
@@ -0,0 +1,13 @@
+import type { Deprecated, Meta } from '../common';
+
+export type PrefixRef< ID extends string = string > = ID & {
+  readonly __brand: 'prefix';
+};
+
+export type PrefixDef< R extends PrefixRef = PrefixRef > = {
+  readonly id: R;
+  factor: number;
+  aliases?: string[];
+  deprecated?: Deprecated< PrefixRef >;
+  meta: Meta;
+};
