cc9d441bd51f55bd74ff26503c4b2a98e02ce684
komed3 <pk97@outlook.de>
2026-05-15 16:08:43 +0200

    describe common types

---
+++ src/types/common.d.ts
@@ -0,0 +1,40 @@
+import type { Format, Lang, UnitSystem } from './dict';
+
+export type LangGroup< T = unknown > =
+  { [ L in Lang ]?: T };
+
+export type FormatGroup< T = unknown > =
+  { [ F in Format.PLAIN ]: T } &
+  { [ F in Exclude< Format, Format.PLAIN > ]?: T };
+
+export type Symbol = {
+  readonly id: string;
+  canonical?: boolean;
+  deprecated?: boolean;
+  context?: {
+    system?: UnitSystem[];
+    lang?: Lang;
+  };
+  format: FormatGroup< string >;
+};
+
+export type Formula = {
+  format: FormatGroup< string >;
+};
+
+export type Name = string | readonly [
+  singular: string, plural?: string
+];
+
+export type Meta = {
+  symbol: Symbol[];
+  formula?: Formula;
+  name?: LangGroup< Name >;
+  description?: LangGroup< string >;
+};
+
+export type Deprecated< R = unknown > = {
+  replacement?: R;
+  since?: string;
+  reason?: LangGroup< string >;
+};
